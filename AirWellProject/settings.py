7fbb4cc35e0f604962bcf3e2c62e39b254aca037
JIKKUMON JAMES <80267142+Jikku34@users.noreply.github.com>
2024-03-08 18:13:41 +0530

    Update settings.py

--- AirWellProject/settings.py
+++ AirWellProject/settings.py
@@ -118,13 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 
-# Additional directories where static files might be located
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
-
-# Directory where Django collects all static files during deployment
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
+else:
+    # Directory where Django collects all static files during deployment
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
